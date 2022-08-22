4ff30eb794c6321cd68e9cefde0bbf70fdc15589
Steve Lee <steve.patriot@gmail.com>
2022-08-22 16:39:04 -0400

    before MultiNavigation for Tab Page All Tab Pages working now. but I need to implement Multi Navigation for Tab Page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:instagram_clone_coding/controller/bottom_navigation_bar_controller.dart';
-import 'package:instagram_clone_coding/pages/about_screen.dart';
-import 'package:instagram_clone_coding/pages/home_screen.dart';
-import 'package:instagram_clone_coding/pages/item_screen.dart';
-import 'package:instagram_clone_coding/pages/like_screen.dart';
-import 'package:instagram_clone_coding/pages/search_screen.dart';
+import 'package:instagram_clone_coding/pages/search_focus.dart';
 import 'package:instagram_clone_coding/wigets/etc.dart';
 
 void main() {
@@ -18,8 +14,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Get.put(BottomNavigationBarController()); // 이게 어디에 들어가야 하는건가? 여기 맞나? 왜 Bindings 객체 상속받은건 안되지?
-     return GetMaterialApp(
+    Get.put(
+        BottomNavigationBarController()); // 이게 어디에 들어가야 하는건가? 여기 맞나? 왜 Bindings 객체 상속받은건 안되지?
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -36,42 +33,87 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print(" 이것도 계속 바뀌나?"); // 아니 안바뀐다. 이렇게 작업하면 일단은 문제가 없다.
-    return Obx(() => // Obx 가 위에 이렇게 있어야 하고.
-       Scaffold(
-        appBar: AppBar(),
-        bottomNavigationBar:
-          BottomNavigationBar(
-            onTap: (index) {
-              print("BottomNavigationNames.values[index] 는 ${BottomNavigationNames.values[index]} 입니다요. ");
-              bottomNavigationBarController.rxTabIndex(BottomNavigationNames.values[index]);},
-            currentIndex: currentIndex(), // 값을 불러와야 하는데.
-            elevation: 0,
-            type: BottomNavigationBarType.fixed,
-            items: bottomNavigationBarItemList(),
-          ),
-         body: currentBody(), // 뭐! 모든게 문제네.. body 하나하는데도 이렇게 시간이 걸린다니.. // 이거 만들때마다 바뀌게 될까?
+    return Obx(
+      () => // Obx 가 위에 이렇게 있어야 하고.
+          Scaffold(
+/*
+        appBar: AppBar(
+          title: Text(
+              bottomNavigationBarController.rxTabIndex.value.name.toString()),
+*/
+/*
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () => Get.back(),),
+*//*
+
+        ),
+*/
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index)=> bottomNavigationBarOnTap(index),
+          currentIndex: currentIndex(),
+          // 값을 불러와야 하는데.
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          items: bottomNavigationBarItemList(),
+        ),
+        body:
+            currentBody(), // 뭐! 모든게 문제네.. body 하나하는데도 이렇게 시간이 걸린다니.. // 이거 만들때마다 바뀌게 될까?
       ),
     );
   }
+
   int currentIndex() {
-    print("controller.rxTabIndex.value.index 는 ${bottomNavigationBarController.rxTabIndex.value.index} 입니다. ");
-    return bottomNavigationBarController.rxTabIndex.value.index; // enum 에서 index 값을 이용해서 BottomNavigationBar 를 Navigation 하고 있다.
+    print(
+        "controller.rxTabIndex.value.index 는 ${bottomNavigationBarController.rxTabIndex.value.index} 입니다. ");
+    return bottomNavigationBarController.rxTabIndex.value
+        .index; // enum 에서 index 값을 이용해서 BottomNavigationBar 를 Navigation 하고 있다.
   }
 
-  Widget currentBody() { // static 으로 만드니깐 더이상 안바뀐다.
+  Widget currentBody() {
+    // static 으로 만드니깐 더이상 안바뀐다.
     switch (bottomNavigationBarController.rxTabIndex.value) {
-      case BottomNavigationNames.HOME : {
-        var home = MyScreens.homeScreen;
-        print("HomeScreen 의 HashCode 는 ${home.hashCode} 입니다.");
-        return home;}
-      case BottomNavigationNames.SEARCH : { return MyScreens.searchScreen;}
-      case BottomNavigationNames.ITEM : { return MyScreens.itemScreen;}
-      case BottomNavigationNames.LIKE : { return MyScreens.likeScreen;}
-      case BottomNavigationNames.ABOUT : { return MyScreens.aboutScreen;}
+      case BottomNavigationNames.HOME:
+        {
+          var home = MyScreens.homeScreen;
+          print("HomeScreen 의 HashCode 는 ${home.hashCode} 입니다.");
+          return home;
+        }
+      case BottomNavigationNames.SEARCH:
+        {
+          return MyScreens.searchScreen;
+        }
+      case BottomNavigationNames.ITEM:
+        {
+          return MyScreens.itemScreen;
+        }
+      case BottomNavigationNames.LIKE:
+        {
+          return MyScreens.likeScreen;
+        }
+      case BottomNavigationNames.ABOUT:
+        {
+          return MyScreens.aboutScreen;
+        }
+    }
   }
+
+  bottomNavigationBarOnTap(int index) {
+    //print("BottomNavigationNames.values[index] 는 ${BottomNavigationNames.values[index]} 입니다요. ");
+    switch (index) {
+      case 2 : {
+        Get.to(SearchFocus());
+        break;
+      }
+      default : {
+
+      }
+    }
+    bottomNavigationBarController
+        .rxTabIndex(BottomNavigationNames.values[index]);
+
   }
 }
-
 
 List<BottomNavigationBarItem> bottomNavigationBarItemList() {
   List<BottomNavigationBarItem> bottomNavigationBarItem = [
--- lib/wigets/etc.dart
+++ lib/wigets/etc.dart
@@ -1,6 +1,6 @@
 import 'package:instagram_clone_coding/pages/about_screen.dart';
 import 'package:instagram_clone_coding/pages/home_screen.dart';
-import 'package:instagram_clone_coding/pages/item_screen.dart';
+import 'package:instagram_clone_coding/pages/search_focus.dart';
 import 'package:instagram_clone_coding/pages/like_screen.dart';
 import 'package:instagram_clone_coding/pages/search_screen.dart';
 
@@ -14,7 +14,7 @@ enum BottomNavigationNames {
 class MyScreens {
   static var aboutScreen = AboutScreen();
   static var homeScreen = HomeScreen();
-  static var itemScreen = ItemScreen();
+  static var itemScreen = SearchFocus();
   static var likeScreen = LikeScreen();
   static var searchScreen = SearchScreen();
 }
