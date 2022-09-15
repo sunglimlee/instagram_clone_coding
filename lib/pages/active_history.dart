8ed23fbcfc6ad9d01ddf5be4786691d23787b6c2
Steve Lee <steve.patriot@gmail.com>
2022-09-15 15:39:26 -0400

    #8, 11:23, active_history.dart - AvatarWidget(thumbPath: 'https://..' 인터넷으로 아바타를 불러오는 부분이네.

--- lib/pages/active_history.dart
+++ lib/pages/active_history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:instagram_clone_coding/widgets/avatar_widget.dart';
 
 class ActiveHistory extends StatelessWidget {
   const ActiveHistory({Key? key}) : super(key: key);
@@ -19,8 +20,83 @@ class ActiveHistory extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            _newRecentlyActiveView(),
+            _newRecentlyThisWeekView(),
+            _newRecentlyThisMonthView(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _newRecentlyActiveView() {
+    Widget _activeItemOne() {
+      // code expand shortcut ctrl + '+' or '-'
+      return Row(
+        children: [
+          AvatarWidget(
+            thumbPath:
+                'https://thumbs.dreamstime.com/b/photo-portrait-cheerful-cool-swag-trend-trendy-guy-leaving-his-feedback-social-media-network-using-smart-phone-photo-145377495.jpg',
+            type: AvatarType.TYPE3,
+          )
+        ],
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      // 집중해라. 그냥 Column 전체 바깥쪽에 공간을 둔다.
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // 전체사용공간 확보로 가로정렬 막아준다.
+        children: [
+          const Text(
+            '오늘',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          _activeItemOne(),
+        ],
+      ),
+    );
+  }
+
+  Widget _newRecentlyThisWeekView() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      // 집중해라. 그냥 Column 전체 바깥쪽에 공간을 둔다.
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // 전체사용공간 확보로 가로정렬 막아준다.
+        children: const [
+          Text(
+            '이번주',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _newRecentlyThisMonthView() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      // 집중해라. 그냥 Column 전체 바깥쪽에 공간을 둔다.
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // 전체사용공간 확보로 가로정렬 막아준다.
+        children: const [
+          Text(
+            '이번달',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
       ),
     );
   }
